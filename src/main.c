c3ad661c0482370105f32eacef2ca12d662b6088
ESBass <149180647+ESBass@users.noreply.github.com>
2024-12-02 17:14:19 +0000

    Added window renderer and background

--- src/main.c
+++ src/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 
+#define HEIGHT 720
+#define WIDTH 1280
+
 int main(int argc, char** argv){
+
+    int shouldClose = 0;
+
+    if(!SDL_Init(SDL_INIT_VIDEO)){
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("WINDOW", WIDTH, HEIGHT, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+    SDL_SetRenderDrawColor(renderer, 7, 0, 87, 255);
+
+    if(!window) return -1;
+
+    while (!shouldClose) {
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev)) {
+            switch (ev.type) {
+                case SDL_EVENT_QUIT:
+                    shouldClose = 1;
+                    break;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     return 0;
 }
