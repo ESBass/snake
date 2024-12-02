1b38f2b1be859c81eb7b43f40322af04931fa401
ESBass <149180647+ESBass@users.noreply.github.com>
2024-12-02 20:22:41 +0000

    There is a snake. It moves.

--- src/main.c
+++ src/main.c
@@ -7,7 +7,12 @@
 #define GAME_X0 140
 #define GAME_Y0 84
 
-void DrawBackground(void);
+#define DIR_RIGHT 0
+#define DIR_LEFT 1
+#define DIR_UP 2
+#define DIR_DOWN 3
+
+#define STEP_SIZE 50
 
 struct {
     SDL_Window* window;
@@ -20,11 +25,15 @@ struct {
 
 struct {
     int x, y;
+    int direction;
 } typedef snake;
 
 gamestate state;
 
 Vector2 WorldSpaceToScreenSpace(Vector2 coords);
+void DrawBackground(void);
+void MoveSnake(snake* s);
+void HandleSnakeInput(snake* s, SDL_Event* event);
 
 int main(int argc, char** argv){
 
@@ -32,6 +41,9 @@ int main(int argc, char** argv){
     int shouldClose = 0;
 
     snake player;
+
+    Vector2 snakeTail[220] = {-1};
+    int snakeLength = 5;
 
     //Init sdl.
     if(!SDL_Init(SDL_INIT_VIDEO)){
@@ -47,6 +59,10 @@ int main(int argc, char** argv){
     player.x = 0;
     player.y = 0;
 
+    player.direction = 0;
+
+    float lastupdate = 0;
+
     while (!shouldClose) {
         SDL_Event ev;
         while (SDL_PollEvent(&ev)) {
@@ -54,14 +70,36 @@ int main(int argc, char** argv){
                 case SDL_EVENT_QUIT:
                     shouldClose = 1;
                     break;
+                case SDL_EVENT_KEY_DOWN:
+                    HandleSnakeInput(&player, &ev);
+            }   
+        }
+
+        if(SDL_GetTicks() - lastupdate > 500){
+            MoveSnake(&player);
+
+            Vector2 prev = {0};
+            Vector2 current = (Vector2){player.x, player.y};
+
+            for(int i = 0; i < (sizeof(snakeTail) / sizeof(Vector2)); i++){
+                prev = snakeTail[i];
+                snakeTail[i] = current;
+                current = prev;
             }
+
+            lastupdate = SDL_GetTicks();
         }
 
         DrawBackground();
 
         SDL_SetRenderDrawColor(state.renderer, 233, 0, 0, 255);
         Vector2 snakePos = WorldSpaceToScreenSpace((Vector2){player.x, player.y});
-        SDL_RenderFillRect(state.renderer, &(SDL_FRect){snakePos.x, snakePos.y, 50, 45});
+        SDL_RenderFillRect(state.renderer, &(SDL_FRect){snakePos.x, snakePos.y, 45, 45});
+
+        for(int seg = 0; seg < snakeLength; seg++){
+            Vector2 segCoords = WorldSpaceToScreenSpace(snakeTail[seg]);
+            SDL_RenderFillRect(state.renderer, &(SDL_FRect){segCoords.x, segCoords.y, 45, 45});
+        }
 
         SDL_RenderPresent(state.renderer);
     }
@@ -89,6 +127,55 @@ void DrawBackground(void)
         }
         currentY += 50;
         currentX = 140;
+    }
+}
+
+void MoveSnake(snake *s)
+{
+    switch (s->direction)
+    {
+    case DIR_RIGHT:
+        s->x += STEP_SIZE;
+        break;
+    case DIR_LEFT:
+        s->x -= STEP_SIZE;
+        break;
+    case DIR_UP:
+        s->y -= STEP_SIZE;
+        break;
+    case DIR_DOWN:
+        s->y += STEP_SIZE;
+        break;
+    default:
+        break;
+    }
+}
+
+void HandleSnakeInput(snake *s, SDL_Event* event)
+{
+    switch (event->key.key)
+    {
+    case SDLK_UP:
+        s->direction = DIR_UP;
+        printf("%s", "UP KEY");
+        break;
+    case SDLK_DOWN:
+        s->direction = DIR_DOWN;
+        printf("%s", "DOWN KEY");
+        break;
+
+    case SDLK_LEFT:
+        s->direction = DIR_LEFT;
+        printf("%s", "KEY LEFT");
+        break;
+
+    case SDLK_RIGHT:
+        s->direction = DIR_RIGHT;
+        printf("%s", "RIGHT KEY");
+        break;
+    
+    default:
+        break;
     }
 }
 
